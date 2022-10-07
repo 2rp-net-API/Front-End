479c87df04c9c7b7090e8ed7a945c1ee4c3a1e0f
Ana Silva <43580298+AnaChristina@users.noreply.github.com>
2022-10-07 10:46:26 -0300

    HoraExtra e Sobreaviso Funcionario
    Paginas Hora extra e sobreaviso funcionário em desenvolvimento, botão atualizar em desenvolvimento

---
+++ Sprint 2/src/components/Sobreaviso/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { alignPropType } from "react-bootstrap/esm/types";
+import "./styles.css"; 
+
+export default function Sobreaviso_funcionario()
+{
+    return(
+    <div className="container"> {/* trabalhar com colunas */}
+        <div classNaeme="">
+            <form className="">
+                <div>  {/* div engloba todo bloco */}
+                    <div className="row">
+                        <div className="col-md col-9">
+                        <input className="form-control rounded-5 text-primary" type="text" id="" placeholder="Usuario"></input>
+                        </div>
+                        <div className="col-md-3">
+                              <input className="form-control rounded-5 text-primary" type="number" id="" placeholder="Matricula"></input>
+                        </div>
+                    </div>
+            
+                    <div className="row">
+                        <div className="col-md-3 mt-5">
+                              <input className="form-control rounded-5 text-primary" type="date" id="" placeholder="Data"></input>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    )
+}
